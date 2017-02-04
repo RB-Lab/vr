bb0464feac78b0240ca0d2792f1201da96a35cb6
bertolomych <rom.bekk@gmail.com>
2017-02-04 22:48:38 +0300

    p2p connection

--- js/index.js
+++ js/index.js
@@ -1,3 +1,14 @@
+import P2P from 'socket.io-p2p';
+import io from 'socket.io-client';
 import startVR from './vr';
 
 startVR();
+
+const socket = io(`${window.location.protocol}//${window.location.hostname}:3050`);
+
+const p2p = new P2P(socket);
+p2p.usePeerConnection = true;
+
+p2p.on('peer-msg', d => console.log('p2p!', d));
+
+window.p2p = p2p;
